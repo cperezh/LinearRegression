b2edd883be5096baa6ba76e8baaf6b629b5030e7
= <=>
2020-04-25 17:50:26 +0200

    add test set

--- housing.py
+++ housing.py
@@ -7,6 +7,7 @@ Created on Fri Apr 10 12:55:49 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import linear_regresion as ln
+import sklearn.model_selection as skl_ms
 
 
 def read_data():
@@ -58,8 +59,8 @@ def plot_data(X, y):
 
 def gradient_descent(X, y):
 
-    alpha = 0.1
-    num_iters = 20000
+    alpha = 1
+    num_iters = 5000
 
     theta, cost_history = ln.gradient_descent(X, y, alpha,
                                               num_iters)
@@ -71,16 +72,10 @@ def gradient_descent(X, y):
 
 def split_data(X, y):
 
-    total_rowa = len(X)
-    learning_rows = int(total_rowa * 0.8)
-    test_rows = int(total_rowa * 0.2)
+    X_train, X_test, y_train, y_test = skl_ms.train_test_split(X, y,
+                                                               test_size=0.2)
 
-    X_learning = X[0:learning_rows, :]
-    X_test = X[learning_rows:, :]
-    y_learning = y[0:learning_rows, :]
-    y_test = y[learning_rows:, :]
-
-    return X_learning, X_test, y_learning, y_test
+    return X_train, X_test, y_train, y_test
 
 
 def model_houseing():
@@ -88,20 +83,18 @@ def model_houseing():
     X, y = read_data()
 
     X = ln.normalize_features(X)
-
-    # Add ones column
+    
+     # Add ones column
     X_new = np.c_[np.ones((len(X), 1)), X]
 
-    # X, X_test, y, y_test = split_data(X, y)
+    X_train, X_test, y_train, y_test = split_data(X_new, y)
 
-    # plot_data(X, y)
+    theta, cost_history = gradient_descent(X_train, y_train)
 
-    theta, cost_history = gradient_descent(X_new, y)
+    cost = ln.calculate_cost(X_test, theta, y_test)
 
-    # cost = ln.calculate_cost(X, theta, y)
-
-    print("Error cuadratico medio: ", float(cost_history[-1]))
     print("Error medio: ", int(np.sqrt(cost_history[-1])))
+    print("Error Test: ",int(np.sqrt(cost)))
 
     # print(cost)
 
@@ -123,15 +116,17 @@ def count_import():
 
         suma = np.sum(solo_i) / len(solo_i)
 
-        agg = np.array([[v, suma]])
-
-        grupo = np.append(grupo, agg, 0)
+        grupo = np.append(grupo, np.array([[v, suma]]), 0)
 
         grupo = grupo[grupo[:,1].argsort(), :]
-
-    plt.bar(grupo[:, 0], grupo[:, 1])
+        
+    y_pos = np.arange(len(grupo))
+        
+    plt.bar(y_pos, grupo[:, 1])
+    
+    plt.xticks(y_pos, grupo[:,0])
 
 
 if __name__ == "__main__":
-    count_import()
-    # model_houseing()
+    # count_import()
+    model_houseing()
