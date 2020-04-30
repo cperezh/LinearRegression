fc0cbee0d67c8f9d0f3dc2a3fe8aa3730b32aa4e
= <=>
2020-04-30 20:37:00 +0200

    added one hot enconding

--- housing.py
+++ housing.py
@@ -14,30 +14,28 @@ def read_data():
     array = np.genfromtxt("data/housing.csv",
                           dtype=float, delimiter=",", skip_header=1,
                           filling_values=0.,
-                          converters={9: convert})
+                          converters={9: convert_ocean_proximity})
 
     labelsColum = 8
-
-    # Delete labels columm
-    X = np.delete(array, labelsColum, 1)
-    
-    
-    X = ln.one_hot_encoding(X,8)
-    
-    X = ln.map_polinomial_features(X,[0,1,2,3,4,5,6,7])
-    
-    # Delete string feature column
-    #X = np.delete(X, 8, 1)
 
     # Read labels column
     y = array[:, labelsColum:labelsColum+1]
 
+    # Delete labels columm
+    X = np.delete(array, labelsColum, 1)
+
+    # One hot over ocean_proximity feature
+    X = ln.one_hot_encoding(X, 8)
+
+    # Build sintetic features
+    X = ln.map_polinomial_features(X, [0, 1, 2, 3, 4, 5, 6, 7])
+
     return X, y
 
 
-def convert(valor):
+def convert_ocean_proximity(valor):
     """
-    Mapeo del campo string a float
+    Mapeo del campo ocean_proximity de string a float
     """
     dict = {
         "NEAR BAY": 0,
@@ -67,8 +65,7 @@ def gradient_descent(X, y):
     alpha = 1
     num_iters = 300
 
-    theta, cost_history = ln.gradient_descent(X, y, alpha,
-                                              num_iters)
+    theta, cost_history = ln.gradient_descent(X, y, alpha, num_iters)
 
     return theta, cost_history
 
@@ -86,37 +83,38 @@ def model_houseing():
     X, y = read_data()
 
     X = ln.normalize_features(X)
-    
+
     # Add ones column
     X_new = np.c_[np.ones((len(X), 1)), X]
 
     X_train, X_test, y_train, y_test = split_data(X_new, y)
-    
+
     # theta, cost_history = gradient_descent(X_train, y_train)
-    
+
     # plt.plot(cost_history)
-    
+
     # print(np.sqrt(cost_history[-1]))
-    
+
     cs_train = np.empty(0)
     cs_test = np.empty(0)
-    
+
     traning_examples = 300
-    
-    for i in range(1,traning_examples):
-          theta, cost_history = gradient_descent(X_train[:i,:], y_train[:i,:])
-          cs_train = np.append(cs_train,np.sqrt(cost_history[-1]))
-          cs_test = np.append(cs_test, np.sqrt(ln.calculate_cost(X_test, theta,
-                                                                y_test)))
-          print(i)
-    
-    plt.plot(range(traning_examples-1),cs_train, label="train")
-    plt.plot(range(traning_examples-1),cs_test, label="test")
-    
+
+    for i in range(1, traning_examples):
+        theta, cost_history = gradient_descent(X_train[:i, :],
+                                               y_train[:i, :])
+        cs_train = np.append(cs_train, np.sqrt(cost_history[-1]))
+        cs_test = np.append(cs_test, np.sqrt(ln.calculate_cost(X_test, theta,
+                                                               y_test)))
+        print(i)
+
+    plt.plot(range(traning_examples-1), cs_train, label="train")
+    plt.plot(range(traning_examples-1), cs_test, label="test")
+
     plt.legend()
- 
+
     print("Error medio train: ", int(cs_train[-1]))
-    print("Error medio Test: ",int(cs_test [-1]))
+    print("Error medio Test: ", int(cs_test[-1]))
 
     # print(cost)
 
@@ -140,13 +138,13 @@ def count_import():
 
         grupo = np.append(grupo, np.array([[v, suma]]), 0)
 
-        grupo = grupo[grupo[:,1].argsort(), :]
-        
+        grupo = grupo[grupo[:, 1].argsort(), :]
+
     y_pos = np.arange(len(grupo))
-        
+
     plt.bar(y_pos, grupo[:, 1])
-    
-    plt.xticks(y_pos, grupo[:,0])
+
+    plt.xticks(y_pos, grupo[:, 0])
 
 
 if __name__ == "__main__":
